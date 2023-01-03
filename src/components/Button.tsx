8222380c8aeeafe00d7cbcbf96f66b8be65ec770
Michał Maciejewski <maciejewskimichal7@gmail.com>
2023-01-04 00:12:40 +0100

    add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import cartIcon from '../../public/icon-cart.svg'
+
+type Props = {
+    text: string,
+    handleAction: () => void,
+}
+
+const Button = (props: Props) => {
+  return (
+    <button
+        className='rounded-[7px] text-white bg-primary hover:bg-primaryDark font-bold p-3 text-center w-full flex justify-center items-center gap-2 transition-all max-w-[300px]'
+        onClick={props.handleAction}
+    >
+        <img src={cartIcon} alt="cart icon" /> {props.text}
+    </button>
+  )
+}
+
+export default Button  
