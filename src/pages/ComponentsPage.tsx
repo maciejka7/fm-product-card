e66dd0cb35a244a5b8a718b5e1a715b81c47ac81
Michał Maciejewski <maciejewskimichal7@gmail.com>
2023-01-08 19:52:36 +0100

    fill components page with text data

--- src/pages/ComponentsPage.tsx
+++ src/pages/ComponentsPage.tsx
@@ -1,37 +1,51 @@
-import React from 'react'
-import {Button, Card, Price} from '../components'
+import React from "react";
+import { Button, Card, Price } from "../components";
+import cardImage from "../../public/image-product-mobile.jpg";
 
-type Props = {}
+type Props = {};
 
-const components: {title: string, component: React.ReactElement}[] = [
+const components: { title: string; component: React.ReactElement }[] = [
   {
-    title: 'Button Component',
-    component: <Button handleAction={() => console.log('button action')} text='Add to cart'/>, 
+    title: "Button Component",
+    component: (
+      <Button
+        handleAction={() => console.log("button action")}
+        text="Add to cart"
+      />
+    ),
   },
   {
-    title: 'Price Component',
+    title: "Price Component",
     component: <Price oldPrice={169.99} currentPrice={149.99} />,
   },
   {
-    title: 'Card Component',
-    component: <Card />
-  }
-]
+    title: "Card Component",
+    component: (
+      <Card
+        title="Gabrielle Essence Eau De Parfum"
+        imgSrc={cardImage}
+        tag="perfume"
+        description="A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL."
+        currentPrice={149.99}
+        oldPrice={169.99}
+        buttonText="Add to cart"
+      />
+    ),
+  },
+];
 
 export const ComponentsPage = (props: Props) => {
   return (
-    <main className='flex flex-col gap-8 p-8'>
-        <h1 className='text-2xl'>Components</h1>
-        {
-          components.map(component => (
-            <section className='border-b pb-4'>
-              <h3 className='mb-2 text-xl'>{component.title}</h3>
-              {component.component}
-             </section>
-          ))
-        }
+    <main className="flex flex-col gap-8 p-8">
+      <h1 className="text-2xl">Components</h1>
+      {components.map((component) => (
+        <section className="border-b pb-4">
+          <h3 className="mb-2 text-xl">{component.title}</h3>
+          {component.component}
+        </section>
+      ))}
     </main>
-  )
-}
+  );
+};
 
-export default ComponentsPage
+export default ComponentsPage;
