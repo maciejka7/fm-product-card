1e6098221a3d78232e896449557390d79ef34ea9
Michał Maciejewski <maciejewskimichal7@gmail.com>
2023-01-09 14:19:51 +0100

    fix some ui

--- src/components/Card/CardDescription.tsx
+++ src/components/Card/CardDescription.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const CardDescription = (props: { description: string; }) => (
-  <p className="text-sm tracking-widest leading-6 text-gray-500 pb-8 xl:pb-9">
+  <p className="text-sm tracking-wider leading-6 text-gray-500 pb-8 xl:pb-9">
     {props.description}
   </p>
 );
--- src/components/Card/CardTag.tsx
+++ src/components/Card/CardTag.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const CardTag = (props: { tag: string; }) => {
-  return <p className="uppercase tracking-[.5rem] text-xs pb-4 xl:pb-6">{props.tag}</p>;
+  return <p className="uppercase tracking-[.5rem] text-xs pb-4 xl:pb-6 text-gray-700">{props.tag}</p>;
 };
